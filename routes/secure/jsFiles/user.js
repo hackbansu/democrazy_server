c315a115b6e4561a7969422e1024f23dd8c95379
Pawan Bansal <bansal.pawan96@gmail.com>
2017-09-04 00:58:26 +0530

    send user details function success

--- routes/secure/jsFiles/user.js
+++ routes/secure/jsFiles/user.js
@@ -3,5 +3,18 @@ const route = express.Router();
 const db = require('./../../../database/JS/db');
 const validateReqParams = require('../../../myJsModules/validation/reqParams');
 
+//function to send user details
+//req.query = {}
+route.get('/getDetails', function (req, res) {
+    let id = req['user']['id'];
+
+    db.users_table.getUsersDetails({id: id}, ['*'], function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(404).json({status: false, msg: "error in database"});
+        }
+        return res.status(200).json({status: true, msg: result[0]});
+    })
+});
 
 module.exports = route;

42b66ae84e417970f2954dd4d3c1b785478552d9
Pawan Bansal <bansal.pawan96@gmail.com>
2017-09-07 17:31:06 +0530

    fixed a bug

--- routes/unSecure/jsFiles/billsOrdinances.js
+++ routes/unSecure/jsFiles/billsOrdinances.js
@@ -4,13 +4,13 @@ const db = require('../../../database/JS/db');
 const validateReqParams = require('../../../myJsModules/validation/reqParams');
 
 //request to get count number of latest bills/ordinances of given states after an offset
-//req.query = {type (0: pending bills, 1: passed bills, 2: enacted ordinances, 3: lapsed ordinances),
+//req.body = {type (0: pending bills, 1: passed bills, 2: enacted ordinances, 3: lapsed ordinances),
 //              state_central_ids: array, offset, count}
-route.get('/billsOrdinances', function (req, res) {
-    let type = parseInt(req.query.type);
-    let state_central_ids = JSON.parse(req.query.state_central_ids);
-    let offset = parseInt(req.query.offset);
-    let count = parseInt(req.query.count);
+route.post('/get', function (req, res) {
+    let type = parseInt(req.body.type);
+    let state_central_ids = JSON.parse(req.body.SCId);
+    let offset = parseInt(req.body.offset);
+    let count = parseInt(req.body.count);
 
     //validation of params (type, state_central_ids, offset, count)
     let integ = state_central_ids.map(function (val, index, arr) {
@@ -20,10 +20,10 @@ route.get('/billsOrdinances', function (req, res) {
     integ.push({val: offset, minVal: 0, maxVal: Number.MAX_SAFE_INTEGER});
     integ.push({val: count, minVal: 1, maxVal: 100});
     let validation = validateReqParams({integ: integ});
-    if (validation) {
-        console.log(validation);
-        return res.status(400).json({status: false, msg: "invalid params"});
-    }
+    // if (validation) {
+    //     console.log(validation);
+    //     return res.status(400).json({status: false, msg: "invalid params"});
+    // }
 
     db.bills_ordinances_table.getBillsAndOrdinances(type, state_central_ids, offset, count, function (err, result) {
         if (err) {
@@ -36,7 +36,7 @@ route.get('/billsOrdinances', function (req, res) {
 
 //request to get details of a bill or an ordinance via id
 //req.query = {id}
-route.get('/billOrdinanceDetails', function (req, res) {
+route.get('/detail', function (req, res) {
     let id = parseInt(req.query.id);
 
     //validation of params (id)
--- routes/unSecure/unSecure.js
+++ routes/unSecure/unSecure.js
@@ -3,14 +3,14 @@ const express = require('express');
 const route = express.Router();
 
 const routes = {
-    billsAndOrdinances: require('./jsFiles/billsAndOrdinances'),
+    billsOrdinances: require('./jsFiles/billsOrdinances'),
     financialInclusions: require('./jsFiles/financialInclusions'),
     opinionPolls: require('./jsFiles/opinionPolls'),
     user: require('./jsFiles/user'),
     locations: require('./jsFiles/locations')
 };
 
-route.use('/billsAndOrdinances', routes.billsAndOrdinances);
+route.use('/billsOrdinances', routes.billsOrdinances);
 route.use('/financialInclusions', routes.financialInclusions);
 route.use('/opinionPolls', routes.opinionPolls);
 route.use('/user', routes.user);
